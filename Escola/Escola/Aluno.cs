0b4985e7929f736c5b396db2db32927bc38aa793
Carlos Guilherme Miranda Finelli <60704748+CarlosFinelli@users.noreply.github.com>
2020-11-09 16:18:43 -0300

    Initial commit

---
+++ Escola/Escola/Aluno.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Escola
+{
+    class Aluno
+    {
+        String Nome, Email, Cidade;
+        Boolean Status;
+        public Aluno()
+        {
+            this.Status = true;
+        }
+        public Aluno(String Nome, String Email, String Cidade) : this()
+        {
+            this.Nome = Nome;
+            this.Email = Email;
+            this.Cidade = Cidade;
+        }
+    }
+}
---
+++ Escola/Escola/Professor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Escola
+{
+    class Professor
+    {
+        String Nome, Sobrenome;
+        int Telefone;
+        Boolean Oculos;
+
+        public Professor()
+        {
+            this.Oculos = true;
+        }
+        public Professor(String Nome, String Sobrenome, int Telefone) : this()
+        {
+            this.Nome = Nome;
+            this.Sobrenome = Sobrenome;
+            this.Telefone = Telefone;
+        }
+
+        public int IncluiTelefone()
+        {
+            return Telefone;
+        }
+
+        public void AtualizaTelefone(int telefone)
+        {
+            this.Telefone = telefone;
+        }
+    }
+}
